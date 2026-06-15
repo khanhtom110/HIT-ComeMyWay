833c4c112aef7cd1c76ecca8f0f150fd27b32868
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2026-06-15 22:13:58 +0700

    setup environent

---
+++ backend/src/main/java/com/hit/comemyway/ComemywayApplication.java
@@ -0,0 +1,13 @@
+package com.hit.comemyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComemywayApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ComemywayApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/com/hit/comemyway/ComemywayApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hit.comemyway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComemywayApplicationTests {
+
+  @Test
+  void contextLoads() {}
+
+}
