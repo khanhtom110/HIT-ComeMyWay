d120af262f8f3f25ec7001956d450dee83d28e37
Nguyen Viet Khanh <lemontre3x1@gmail.com>
2026-06-16 21:12:33 +0700

    refactor: change RoleConstant

--- backend/src/main/java/com/hit/comemyway/constant/RoleConstant.java
+++ backend/src/main/java/com/hit/comemyway/constant/RoleConstant.java
@@ -2,6 +2,6 @@ package com.hit.comemyway.constant;
 
 public class RoleConstant {
     private RoleConstant() {}
-    public static final String ADMIN = "ROLE_ADMIN";
+    public static final String CLINIC = "ROLE_CLINIC";
     public static final String USER = "ROLE_USER";
 }
