1b13d8a177c09bdd5d6b511e4e10f1e0c0be9620
NguyenTienDat-tech <khaidaikhai222@gmail.com>
2026-07-26 17:17:59 +0700

    fix: constaint and register

--- frontend/app/src/main/java/com/vetpet/petbeats/core/network/ApiConstants.kt
+++ frontend/app/src/main/java/com/vetpet/petbeats/core/network/ApiConstants.kt
@@ -7,13 +7,13 @@ object ApiConstants {
 
 
     //Authentication
-    const val LOGOUT = "api/v1/auth/logout"
+    const val LOGOUT = "/api/v1/auth/logout"
     const val RESETOTP = "/api/v1/auth/verify-otp"
     const val REGISTEROTP = "/api/v1/auth/verify-register"
     const val RESETPASSWORD = "/api/v1/auth/reset-password"
-    const val REGISTER = "api/v1/auth/register"
+    const val REGISTER = "/api/v1/auth/register"
     const val REFRESH = "/api/v1/auth/refresh"
-    const val LOGIN = "api/v1/auth/login"
+    const val LOGIN = "/api/v1/auth/login"
     const val FORGOTPASSWORD = "/api/v1/auth/forgot-password"
 
 
