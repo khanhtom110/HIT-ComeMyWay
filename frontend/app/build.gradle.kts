e24bdbc2472f7aa21f9f40091ad3b47cce06952d
NguyenTienDat-tech <khaidaikhai222@gmail.com>
2026-08-01 16:07:05 +0700

    feat: update project configuration and navigation graph for locket feature

--- frontend/app/build.gradle.kts
+++ frontend/app/build.gradle.kts
@@ -91,4 +91,11 @@ dependencies {
     implementation(platform("com.google.firebase:firebase-bom:34.16.0"))
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-messaging")
+
+
+    val camerax_version = "1.3.0" // Hoặc mới hơn
+    implementation("androidx.camera:camera-core:${camerax_version}")
+    implementation("androidx.camera:camera-camera2:${camerax_version}")
+    implementation("androidx.camera:camera-lifecycle:${camerax_version}")
+    implementation("androidx.camera:camera-view:${camerax_version}")
 }
