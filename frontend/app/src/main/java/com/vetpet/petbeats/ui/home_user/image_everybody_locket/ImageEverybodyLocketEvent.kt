6a4a6804e24331ff0d3f2b5bc7909cdaaad1f5c3
NguyenTienDat-tech <khaidaikhai222@gmail.com>
2026-08-06 10:32:13 +0700

    feat: test

--- frontend/app/src/main/java/com/vetpet/petbeats/ui/home_user/image_everybody_locket/ImageEverybodyLocketEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.vetpet.petbeats.ui.home_user.image_everybody_locket
-
-sealed class ImageEverybodyLocketEvent {
-    object NavigationImageEverybodyLocket: ImageEverybodyLocketEvent()
-}
--- frontend/app/src/main/java/com/vetpet/petbeats/ui/home_user/image_me_locket/ImageMeLocketEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.vetpet.petbeats.ui.home_user.image_me_locket
-
-sealed class ImageMeLocketEvent {
-    object NavigationImageMeLocket: ImageMeLocketEvent()
-}
